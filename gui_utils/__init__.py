d39c71b9dd1fb009efbcded012b7c99a5f47508d
SauceBoss1 <dtvsworld@gmail.com>
2024-02-09 19:46:15 -0800

    next steps: compile

--- gui_utils/__init__.py
+++ gui_utils/__init__.py
@@ -37,19 +37,39 @@ class BrowseFile(ctk.CTkFrame):
 class Options(ctk.CTkFrame):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.optionsKwargs = {
-            "384": True,
-            "long2sq": True,
-        }
+        self.optionsKwargs = {"384": True, "long2sq": True, "index": 0}
         self.text = ctk.CTkLabel(
             master=self, text="Options", font=("Arial", 25, "bold")
         )
-        self.text.pack(side="top", padx=15, pady=(10, 5))
+        self.text.pack(side="top", padx=15, pady=4)
+
+        ## Index Specification ##
+        self.indexInput = ctk.StringVar()
+        self.indexFrame = ctk.CTkFrame(master=self, width=300)
+        self.indexFrame.pack(
+            side="top", padx=15, pady=(5, 10), expand=False, anchor="center", fill="x"
+        )
+
+        self.indexText = ctk.CTkLabel(
+            master=self.indexFrame, text="Index Column Name", font=("Arial", 14, "bold")
+        )
+        self.indexText.pack(side="left", expand=False, padx=(10, 5), pady=5)
+
+        self.indexEntry = ctk.CTkEntry(
+            master=self.indexFrame,
+            placeholder_text="Index Columns",
+            textvariable=self.indexInput,
+        )
+        self.indexEntry.pack(side="right", fill="x", expand=True, padx=(5, 10), pady=5)
+
+        ## Button Group ##
+        self.buttonGroupFrame = ctk.CTkFrame(master=self)
+        self.buttonGroupFrame.pack(side="top", fill="both", expand=True)
 
         ## Well type selection ##
-        self.wellTypeFrame = ctk.CTkFrame(master=self)
+        self.wellTypeFrame = ctk.CTkFrame(master=self.buttonGroupFrame)
         self.wellTypeFrame.pack(
-            side="left", padx=10, pady=15, expand=True, fill="x", anchor="center"
+            side="left", padx=10, pady=10, expand=True, fill="both", anchor="center"
         )
 
         self.wellTypeText = ctk.CTkLabel(
@@ -58,12 +78,12 @@ class Options(ctk.CTkFrame):
         self.wellTypeText.pack(side="top", anchor="center", pady=10, padx=10)
 
         # buttons below
-        self.selectedWellType = ctk.StringVar(value="None")
+        self.selectedWellType = ctk.StringVar(value="384")
         self.btn_384 = ctk.CTkRadioButton(
             master=self.wellTypeFrame,
             width=100,
             height=20,
-            value=384,
+            value="384",
             text="384 well type",
             command=lambda: self.changeOption(opt_384=True),
             variable=self.selectedWellType,
@@ -74,7 +94,7 @@ class Options(ctk.CTkFrame):
             master=self.wellTypeFrame,
             width=100,
             height=20,
-            value=96,
+            value="96",
             text="96 well type",
             command=lambda: self.changeOption(opt_384=False),
             variable=self.selectedWellType,
@@ -82,9 +102,9 @@ class Options(ctk.CTkFrame):
         self.btn_96.pack(side="top", anchor="center", padx=5, pady=5)
 
         ## Input type selection ##
-        self.inputTypeFrame = ctk.CTkFrame(master=self)
+        self.inputTypeFrame = ctk.CTkFrame(master=self.buttonGroupFrame)
         self.inputTypeFrame.pack(
-            side="right", padx=10, pady=15, expand=True, fill="x", anchor="center"
+            side="right", padx=10, pady=10, expand=True, fill="both", anchor="center"
         )
 
         self.inputTypeText = ctk.CTkLabel(
@@ -93,7 +113,7 @@ class Options(ctk.CTkFrame):
         self.inputTypeText.pack(side="top", anchor="center", pady=10, padx=10)
 
         # buttons below
-        self.btn_inputTypeVar = ctk.StringVar(value="None")
+        self.btn_inputTypeVar = ctk.StringVar(value="l2s")
         self.btn_long2square = ctk.CTkRadioButton(
             master=self.inputTypeFrame,
             text="List -> Square",
@@ -116,13 +136,14 @@ class Options(ctk.CTkFrame):
         )
         self.btn_square2long.pack(side="top", anchor="center", padx=5, pady=5)
 
-    def changeOption(self, opt_384: bool = True, opt_long2sq: bool = True) -> None:
+    def changeOption(
+        self, opt_384: bool = True, opt_long2sq: bool = True, index: str = "384_well"
+    ) -> None:
         if not opt_384:
             self.optionsKwargs["384"] = False
 
         if not opt_long2sq:
             self.optionsKwargs["long2sq"] = False
-        print(self.optionsKwargs)
 
     @property
     def getOptions(self):
--- simplePlatemapConverter.py
+++ simplePlatemapConverter.py
@@ -5,9 +5,15 @@ import os, sys
 import customtkinter as ctk
 from converterModules import *
 from gui_utils import *
+import tkinter as tk
+from tkinter import messagebox
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
+
+
+def errorMessage(message: str):
+    messagebox.showerror(title="Error", message=message)
 
 
 class App(ctk.CTk):
@@ -40,7 +46,50 @@ class App(ctk.CTk):
 
     def submit(self):
         inFile = self.browse.getFilePath
-        print(inFile)
+        if inFile == "":
+            errorMessage(message="No file Input")
+        else:
+            inFileExt = os.path.splitext(inFile)[-1]
+            options = self.options.getOptions
+            options["index"] = self.options.indexInput.get()
+            if not options["384"]:
+                width, height = 12, 8
+            else:
+                width, height = 24, 16
+
+            if options["index"] == "":
+                errorMessage(message="Please specify the index column")
+            else:
+                if options["long2sq"]:
+                    outFile = self.downloadProcess(type=".xlsx")
+                else:
+                    outFile = self.downloadProcess()
+
+                if not options["long2sq"]:
+                    df = sqaure2platemap(inFile=inFile, index=options["index"])
+                    df.to_csv(outFile)
+                elif options["long2sq"]:
+                    df = (
+                        pd.read_csv(inFile)
+                        if inFileExt == ".csv"
+                        else pd.read_excel(inFile)
+                    )
+
+                    if isSquare(df=df):
+                        errorMessage(message="The file input is already a square")
+                        pass
+                    else:
+                        platemap2square(
+                            df=df,
+                            index=options["index"],
+                            outPath=outFile,
+                            width=width,
+                            height=height,
+                        )
+
+    def downloadProcess(self, type=".csv"):
+        filePath = ctk.filedialog.asksaveasfilename(defaultextension=type)
+        return filePath
 
 
 if __name__ == "__main__":
